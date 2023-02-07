cb6309e0fdeb7e7b0093e6a2847c72899d2ecd0e
gaianardella <105795404+gaia-nardella@users.noreply.github.com>
2023-02-07 21:50:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,35 @@
-import streamlit
-import pandas
-import requests
-from urllib.error import URLError
+import streamlit as st
+import streamlit_authenticator as stauth
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import streamlit
+# import pandas
+# import requests
+# from urllib.error import URLError
+
+# streamlit.title('My Parents New Healthy Diner')
+# streamlit.header('Breakfast Menu')
+# streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+# streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+# streamlit.text('🥑🍞 Avocado Toast')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # my_fruit_list = my_fruit_list.set_index('Fruit')
