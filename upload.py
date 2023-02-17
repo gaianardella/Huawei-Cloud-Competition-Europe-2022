d52ccdc84ebea19f366bd9ad2f25da32db580672
gaianardella <105795404+gaia-nardella@users.noreply.github.com>
2023-02-17 10:32:20 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,6 +1,24 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from plyer import filechooser
+from obs import ObsClient, CorsRule, Options
+from datetime import datetime
+# import huaweicloud
+import boto3
+import os
+
+# from huaweicloudsdkobs.obs_client import ObsClient
+from huaweicloudsdkcore.auth.credentials import BasicCredentials
+
+# Replace these with your own values
+endpoint = "http://obs.eu-west-101.myhuaweicloud.eu"
+server="obs.eu-west-101.myhuaweicloud.eu"
+access_key = "JAKGEFBQBLHWUNWWGIGI"
+secret_key = "3lRysHwqAlRnw7kVFSWRCZFuIbdHau6UYHfzObz3"
+bucket_name = "clothes"
+expire_seconds = 3600
+
+obsClient = ObsClient(access_key_id='', secret_access_key='', server='obs.eu-west-101.myhuaweicloud.eu')
 
 kv="""
 MDFloatLayout:
@@ -28,6 +46,27 @@ class FileChooser(MDApp):
     def selected(self, selection):
         if selection:
             self.root.ids.img.source = selection[0]
+
+            print(selection[0]) #è il path della foto selezionata
+            file_path = selection[0]
+
+            print(type(file_path))
+            #bucketClient = obsClient.bucketClient('clothes')
+        
+            # Getting the current date and time
+            dt = datetime.now()
+            # getting the timestamp
+            ts = datetime.timestamp(dt)
+
+            # resp = bucketClient.putContent('top' + str(ts), file.read())
+            #resp = obsClient.putFile('clothes', 'top', file)
+
+            obsClient.putFile('clothes',str(ts), file_path)
+
+
+            #kivy_venv\Scripts\activate
+
+
 
 
 
